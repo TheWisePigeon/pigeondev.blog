ebc855b8cfcae5b6bdaf71ed6d2b469b56d6577e
TheWisePigeon <zozozozeph@gmail.com>
2022-08-07 20:55:31 +0000

    styled sub button| added hover effect

--- pages/sub.jsx
+++ pages/sub.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 
 export default function Sub() {
   return (
-    <>
-        <div>
+    <div>
             <p>Subscribe to my newsletter, I won't spam you 😇</p>
             <form action="" method="post">
                 <input type="text" name="sub" id="sub" placeholder='yourmail@domain.com' className='rounded' />
             </form>
             <button  className='sub' >Subscribe</button>
-        </div>
-    </>
+    </div>
   )
 }
