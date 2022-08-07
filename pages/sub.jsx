3de1b48a97692d46dea4eb8841ebbe695e03b87f
TheWisePigeon <zozozozeph@gmail.com>
2022-08-07 20:09:47 +0000

    added subscription to mail list component

---
+++ pages/sub.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Sub() {
+  return (
+    <>
+        <div>
+            <p>Subscribe to my newsletter, I won't spam you :)</p>
+            <form action="" method="post">
+                <input type="text" name="sub" id="sub" placeholder='yourmail@domain.com' className='' />
+            </form>
+        </div>
+    </>
+  )
+}
