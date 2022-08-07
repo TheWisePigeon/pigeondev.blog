7bdfaca15f5763ea32770bc901269553357e63ee
TheWisePigeon <zozozozeph@gmail.com>
2022-08-07 20:35:30 +0000

    styled sub button

--- pages/sub.jsx
+++ pages/sub.jsx
@@ -4,10 +4,11 @@ export default function Sub() {
   return (
     <>
         <div>
-            <p>Subscribe to my newsletter, I won't spam you :)</p>
+            <p>Subscribe to my newsletter, I won't spam you 😇</p>
             <form action="" method="post">
                 <input type="text" name="sub" id="sub" placeholder='yourmail@domain.com' className='rounded' />
             </form>
+            <button  className='sub' >Subscribe</button>
         </div>
     </>
   )
