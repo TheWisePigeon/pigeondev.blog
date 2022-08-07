c7f06a6118acc92ea99e4b294a308702e4e27eaa
TheWisePigeon <zozozozeph@gmail.com>
2022-08-07 20:59:48 +0000

    added subs.json

--- pages/sub.jsx
+++ pages/sub.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+const subs = require('../public/subs.json')
 
 export default function Sub() {
+    function handleFormSubmit(event){
+        event.preventDefault()
+        subs.subs.addresses.push("duh")
+    }
+
+
+
   return (
     <div>
             <p>Subscribe to my newsletter, I won't spam you 😇</p>
-            <form action="" method="post">
+            <form action="" onSubmit={handleFormSubmit} method="post">
                 <input type="text" name="sub" id="sub" placeholder='yourmail@domain.com' className='rounded' />
             </form>
-            <button  className='sub' >Subscribe</button>
+            <button type='submit'  className='sub' >Subscribe</button>
     </div>
   )
 }
