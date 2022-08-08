838a60bad49a8900ac63adb69d7751392342a8d0
TheWisePigeon <zozozozeph@gmail.com>
2022-08-08 00:58:42 +0000

    added route to sub

--- pages/sub.jsx
+++ pages/sub.jsx
@@ -1,21 +1,41 @@
 import React from 'react'
-const subs = require('../public/subs.json')
+import axios from 'axios'
+import { useState } from 'react'
 
 export default function Sub() {
-    function handleFormSubmit(event){
+
+    const [loading, setLoading] = useState(false)
+    async function handleFormSubmit(event) {
+        setLoading(true)
         event.preventDefault()
-        subs.subs.addresses.push("duh")
+        const result = axios.post(
+            'https://serverless-pigeon.vercel.app/api/store_sub',
+            {
+                "newSub":event.target.sub.value
+            }
+        ).then(
+            (res)=>{
+                setLoading(false)
+                console.log(res);
+            }
+        ).catch(
+            err=>{
+                console.log(err);
+            }
+        )
     }
 
 
-
-  return (
-    <div>
+    return (
+        <div>
             <p>Subscribe to my newsletter, I won't spam you 😇</p>
             <form action="" onSubmit={handleFormSubmit} method="post">
-                <input type="text" name="sub" id="sub" placeholder='yourmail@domain.com' className='rounded' />
+                <input type="text" name="sub" required id="sub" placeholder='yourmail@domain.com' className='rounded' /><br />
+                <button type="submit" className='sub'>
+                    {loading?"...":"Subscribe"}
+                </button>
             </form>
-            <button type='submit'  className='sub' >Subscribe</button>
-    </div>
-  )
+
+        </div>
+    )
 }
